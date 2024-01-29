562473091bd41f02ba46e9e353047f15bda7de36
josegarcia19809 <josegarcia19809@gmail.com>
2024-01-29 10:42:23 -0600

    Creación de un GameManager

---
+++ GameManager.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    [SerializeField] private GameObject player;
+    private bool gameOver = false;
+
+    public GameObject Player
+    {
+        get { return Player; }
+    }
+
+    public bool GameOver
+    {
+        get { return gameOver; }
+    }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void PlayerHit(int currentHP)
+    {
+        if (currentHP > 0)
+        {
+            gameOver = false;
+        }
+        else
+        {
+            gameOver = true;
+        }
+    }
+}
