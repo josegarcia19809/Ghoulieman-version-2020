2f27034820acdc7a5dc69e87def8511df8be4a0d
josegarcia19809 <josegarcia19809@gmail.com>
2024-12-01 17:53:23 -0600

    Reaparición del Player II

--- LevelManager.cs
+++ LevelManager.cs
@@ -4,20 +4,33 @@ using UnityEngine;
 
 public class LevelManager : MonoBehaviour
 {
+    public float timer = 0f;
+    public float waitTime = 2.0f;
+
+    public GameObject currentCheckpoint;
+    private GameObject player;
+    private PlayerHealth playerHealth;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = GameManager.instance.Player;
+        playerHealth = player.GetComponent<PlayerHealth>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void RespawnPlayer()
     {
-        print("Player Respawn");
+        timer += Time.deltaTime;
+        if (timer >= waitTime)
+        {
+            print("Player Respawn");
+            player.transform.position = currentCheckpoint.transform.position;
+            playerHealth.CurrentHealth = 100;
+        }
     }
 }
