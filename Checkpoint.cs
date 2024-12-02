4e58627553e71a660a13d8336cd6cc311dfabf67
josegarcia19809 <josegarcia19809@gmail.com>
2024-12-01 18:53:21 -0600

    Programación de más checkpoints

--- Checkpoint.cs
+++ Checkpoint.cs
@@ -1,18 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    public LevelManager levelManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        levelManager = FindObjectOfType<LevelManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.name == "Player")
+        {
+            levelManager.currentCheckpoint = gameObject;
+        }
     }
 }
