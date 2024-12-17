2d9285ba788064a6526e4a8d210ec35e45666f8b
josegarcia19809 <josegarcia19809@gmail.com>
2024-12-17 12:09:44 -0600

    Programación de Spawner II

--- SpawnerScripts/Cave01Health.cs
+++ SpawnerScripts/Cave01Health.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,27 +10,27 @@ public class Cave01Health : MonoBehaviour
     [SerializeField] private float timeSinceLastHit = 0.2f;
     [SerializeField] private float dissapearSpeed = 2f;
 
-    private float timer=0f;
+    private float timer = 0f;
     private Animator anim;
     private bool isAlive;
     private new Rigidbody rigidbody;
     private BoxCollider boxCollider;
-    private bool dissapearEnemy  = false;
-    
+    private bool dissapearEnemy = false;
+
     private new AudioSource audio;
-    private AudioClip hurtAudio;
-    private AudioClip killAudio;
-    
+    public AudioClip hurtAudio;
+    public AudioClip killAudio;
+
     private DropItems dropItem;
-    
+
     public GameObject explosionEffect;
 
     public bool IsAlive
     {
         get { return isAlive; }
     }
-    
-    
+
+
     void Start()
     {
         rigidbody = GetComponent<Rigidbody>();
@@ -44,6 +45,54 @@ public class Cave01Health : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timer += Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (timer >= timeSinceLastHit && !GameManager.instance.GameOver)
+        {
+            if (other.CompareTag("PlayerWeapon"))
+            {
+                TakeHit();
+                timer = 0f;
+            }
+        }
+    }
+
+    void TakeHit()
+    {
+        if (currentHealth > 0)
+        {
+            GameObject newExplosionEffect = (GameObject)Instantiate(explosionEffect,
+                transform.position, transform.rotation);
+            Destroy(newExplosionEffect, 1);
+
+            anim.Play("SpawnerHurt");
+            currentHealth -= 10;
+            audio.PlayOneShot(hurtAudio);
+        }
+
+        if (currentHealth <= 0)
+        {
+            isAlive = false;
+            KillEnemy();
+        }
+    }
+
+    void KillEnemy()
+    {
+        boxCollider.enabled = false;
+        anim.SetTrigger("EnemyDie");
+        audio.PlayOneShot(killAudio);
+
+        StartCoroutine(RemoveEnemy());
+        dropItem.Drop();
+    }
+
+    IEnumerator RemoveEnemy()
+    {
+        yield return new WaitForSeconds(2f);
+        Destroy(gameObject);
     }
 }
