988c14f3ebfef3c8ed0c21996efcfabf0c7b5345
Jose Garcia <josegarcia19809@gmail.com>
2024-12-30 10:34:35 -0600

    Animación de daño a boss

--- BossScripts/BossHealth.cs
+++ BossScripts/BossHealth.cs
@@ -6,10 +6,12 @@ using UnityEngine;
 public class BossHealth : MonoBehaviour
 {
     public int bossHealth = 20;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
+        animator = GameObject.Find("Boss"). GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -21,6 +23,7 @@ public class BossHealth : MonoBehaviour
     {
         if (other.CompareTag("PlayerWeapon") && bossHealth > 0)
         {
+            animator.SetTrigger("isHit");
             bossHealth--;
             print("Boss Health: " + bossHealth);
         }
