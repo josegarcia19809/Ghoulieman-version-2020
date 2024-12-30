736a612c4f041372a95bc31444b78f097e9ca0d0
Jose Garcia <josegarcia19809@gmail.com>
2024-12-30 14:49:09 -0600

    Trabajo con video

--- BossScripts/BossHealth.cs
+++ BossScripts/BossHealth.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BossHealth : MonoBehaviour
 {
@@ -16,6 +17,8 @@ public class BossHealth : MonoBehaviour
 
     public Material hurtBossMaterial;
     private GameObject bossModel;
+    
+    public GameObject videoPlayer;
 
     // Start is called before the first frame update
     void Start()
@@ -26,6 +29,7 @@ public class BossHealth : MonoBehaviour
         weaponCollider = GameObject.Find("Boss").GetComponentInChildren<BoxCollider>();
         triggerCollider = GameObject.Find("Boss").GetComponentInChildren<SphereCollider>();
         bossModel = GameObject.FindGameObjectWithTag("BossModel");
+        videoPlayer.SetActive(false);
     }
 
     // Update is called once per frame
@@ -58,5 +62,15 @@ public class BossHealth : MonoBehaviour
         capsuleCollider.enabled = false;
         weaponCollider.enabled = false;
         triggerCollider.enabled = false;
+
+        StartCoroutine(PlayVideo());
+    }
+
+    IEnumerator PlayVideo()
+    {
+        yield return new WaitForSeconds(5);
+        videoPlayer.SetActive(true);
+        yield return new WaitForSeconds(7);
+        SceneManager.LoadScene(0);
     }
 }
